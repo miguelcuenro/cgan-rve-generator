c30404dbde855d863548e04e1205e41a62d0bdb3
Miwel <mcuencarodriguez2001@gmail.com>
2026-02-06 14:48:21 +0100

    Add visualize_npy function to vis utils

--- src/utils/visualization_utils.py
+++ src/utils/visualization_utils.py
@@ -50,3 +50,19 @@ def visualize_and_log_to_tensorboard(tag, tensor, step, writer, colormap='gist_e
     img_tensor = np.transpose(img, (2, 0, 1))
     # img_tensor = torch.tensor(img_tensor, dtype=torch.float32) / 255.0  # Normalize to [0, 1]
     writer.add_image(tag, img_tensor, global_step=step, dataformats='CHW')
+
+def visualize_npy_file(filepath, colormap='gist_earth'):
+    npy_data = np.load(filepath)
+    grid = pv.ImageData(dimensions=(npy_data.shape[0] + 1, npy_data.shape[1] + 1, npy_data.shape[2] + 1),
+                        spacing=(1, 1, 1))
+    grid.cell_data["values"] = npy_data.flatten(order="F")  # Assigning the cell data
+
+    camera_position = [
+        (200, 200, 200),  # Camera location
+        (32, 32, 32),  # Focal point
+        (0, 0, 1)  # View up direction
+    ]
+
+    # Plotting
+    grid.plot(full_screen=True, screenshot=True, cmap=colormap, background="white", show_edges=False, show_grid=True,
+              cpos=camera_position)
