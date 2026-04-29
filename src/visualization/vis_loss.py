2a68bbd5ccb4f88832c64a511ae3511eae8dbb08
Miwel <mcuencarodriguez2001@gmail.com>
2026-04-29 11:22:18 +0800

    Add main guard to vis_loss and vis_rve

--- src/visualization/vis_loss.py
+++ src/visualization/vis_loss.py
@@ -10,30 +10,9 @@ import yaml
 
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
-# Load the parameters file
-with open('parameters.yaml', 'r') as dictionary:
-    parameters = yaml.safe_load(dictionary)
-
-# Raise an error if parameters.yaml is NOT a dictionary
-if not isinstance(parameters, dict):
-    raise TypeError("parameters.yaml did not parse to a dictionary. Please check the file's structure.")
-
-event_dir =os.path.expanduser(parameters['event_dir'])
-dataroot = os.path.expanduser(parameters['dataroot'])
-image_dir = os.path.join(dataroot, "img")
-
-# Guidelines
-print("Generator Loss:")
-print("Expect a Plateau. You aren't looking for this to go to zero. Once it stabilizes, it means the Generator has found a consistent way to fool the Critic.")
-print(15*'-')
-print('Critic Loss:')
-print('Expect Stable Negative Values. Since the Critic is minimizing the negative Wasserstein distance, it should hover in a stable negative range. Constant diving into deep negative numbers indicates instability.')
-print(15*'-')
-print('Gradient Penalty (GP):')
-print('Expect Near-Zero. This is your safety rail. If this value starts climbing rapidly, your training is becoming "unconstrained," and your gradients are likely about to explode.')
-print(15*'-')
-print('Real vs. Fake Gap:')
-print('Expect a consistent, positive gap; the Real samples should remain above the Fake samples, and as training progresses, you want the Fake line to trend upward toward the Real line until the distance stabilizes at a low value.')
+# -------------------- #
+# FUNCTION DEFINITIONS
+# -------------------- #
 
 def read_tensorboard_logs(log_path, tag):
     # Initialize the event accumulator
@@ -75,85 +54,117 @@ def moving_avg(data, window_size=10):
     ma = np.convolve(pad_data, np.ones(window_size), mode='same') / window_size
 
     return ma
-    
 
-# Usage
-log_dir = event_dir + "/events.out.tfevents.1775907087.MacBook-Pro-de-Miguel.local.8267.0" 
+# -------------- #
+# MAIN EXECUTION
+# -------------- #
 
-print_available_tags(log_dir)
+def main():
+    # Load the parameters file
+    with open('parameters.yaml', 'r') as dictionary:
+        parameters = yaml.safe_load(dictionary)
 
-steps_dis, losses_dis = read_tensorboard_logs(log_dir, "Loss/Critic")
+    # Raise an error if parameters.yaml is NOT a dictionary
+    if not isinstance(parameters, dict):
+        raise TypeError("parameters.yaml did not parse to a dictionary. Please check the file's structure.")
 
-steps_fake, losses_fake = read_tensorboard_logs(log_dir, "Loss/D_fake")
-steps_real, losses_real = read_tensorboard_logs(log_dir, "Loss/D_real")
+    event_dir =os.path.expanduser(parameters['event_dir'])
+    dataroot = os.path.expanduser(parameters['dataroot'])
+    image_dir = os.path.join(dataroot, "img")
 
-steps_gen, losses_gen = read_tensorboard_logs(log_dir, "Loss/Generator")
+    # Guidelines
+    print("Generator Loss:")
+    print("Expect a Plateau. You aren't looking for this to go to zero. Once it stabilizes, it means the Generator has found a consistent way to fool the Critic.")
+    print(15*'-')
+    print('Critic Loss:')
+    print('Expect Stable Negative Values. Since the Critic is minimizing the negative Wasserstein distance, it should hover in a stable negative range. Constant diving into deep negative numbers indicates instability.')
+    print(15*'-')
+    print('Gradient Penalty (GP):')
+    print('Expect Near-Zero. This is your safety rail. If this value starts climbing rapidly, your training is becoming "unconstrained," and your gradients are likely about to explode.')
+    print(15*'-')
+    print('Real vs. Fake Gap:')
+    print('Expect a consistent, positive gap; the Real samples should remain above the Fake samples, and as training progresses, you want the Fake line to trend upward toward the Real line until the distance stabilizes at a low value.')
 
-steps_gp, losses_gp = read_tensorboard_logs(log_dir, "GP")
+    # Usage
+    log_dir = event_dir + "/events.out.tfevents.1775907087.MacBook-Pro-de-Miguel.local.8267.0" 
 
-# Create a 2x2 grid of subplots
-fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+    print_available_tags(log_dir)
 
-# Flatten the 2D array of axes to make indexing easier (0, 1, 2)
-axs_flat = axs.flatten()
+    steps_dis, losses_dis = read_tensorboard_logs(log_dir, "Loss/Critic")
 
-# Plotting the three subplots
-axs_flat[0].plot(steps_gen, losses_gen, label="Gen Loss")
-axs_flat[0].plot(steps_gen, moving_avg(data=losses_gen, window_size=10)[9:-9], linestyle='--', label='Centered MA')
-axs_flat[0].hlines(y=0, xmin=steps_gen[0]-1e2, xmax=steps_gen[-1]+1e2, linestyle='--', color='r', alpha=0.6)
+    steps_fake, losses_fake = read_tensorboard_logs(log_dir, "Loss/D_fake")
+    steps_real, losses_real = read_tensorboard_logs(log_dir, "Loss/D_real")
 
-axs_flat[0].set_xlabel('Iteration')
-axs_flat[0].set_ylabel('Loss Score')
-axs_flat[0].set_xlim(-5, steps_gen[-1]+5)
+    steps_gen, losses_gen = read_tensorboard_logs(log_dir, "Loss/Generator")
 
-axs_flat[0].legend()
-axs_flat[0].set_title('Subplot 1: Generator Loss')
+    steps_gp, losses_gp = read_tensorboard_logs(log_dir, "GP")
 
-axs_flat[1].plot(steps_dis, losses_dis, label='Critic Loss')
-axs_flat[1].plot(steps_dis, moving_avg(data=losses_dis, window_size=10)[9:-9], linestyle='--', label='Centered MA')
-axs_flat[1].hlines(y=0, xmin=steps_dis[0]-1e2, xmax=steps_dis[-1]+1e2, linestyle='--', color='r', alpha=0.6)
+    # Create a 2x2 grid of subplots
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
 
-axs_flat[1].set_xlabel('Iteration')
-axs_flat[1].set_ylabel('Loss Score')
-axs_flat[1].set_xlim(-5, steps_dis[-1]+5)
+    # Flatten the 2D array of axes to make indexing easier (0, 1, 2)
+    axs_flat = axs.flatten()
 
-axs_flat[1].legend()
-axs_flat[1].set_title('Subplot 2: Critic Loss')
+    # Plotting the three subplots
+    axs_flat[0].plot(steps_gen, losses_gen, label="Gen Loss")
+    axs_flat[0].plot(steps_gen, moving_avg(data=losses_gen, window_size=10)[9:-9], linestyle='--', label='Centered MA')
+    axs_flat[0].hlines(y=0, xmin=steps_gen[0]-1e2, xmax=steps_gen[-1]+1e2, linestyle='--', color='r', alpha=0.6)
 
-axs_flat[2].plot(steps_gp, losses_gp, label='')
-axs_flat[2].hlines(y=0, xmin=steps_gp[0]-1e2, xmax=steps_gp[-1]+1e2, linestyle='--', color='r', alpha=0.6)
+    axs_flat[0].set_xlabel('Iteration')
+    axs_flat[0].set_ylabel('Loss Score')
+    axs_flat[0].set_xlim(-5, steps_gen[-1]+5)
 
-axs_flat[2].set_xlabel('Iteration')
-axs_flat[2].set_ylabel('Penalty Norm')
-axs_flat[2].set_xlim(-5, steps_gp[-1]+5)
+    axs_flat[0].legend()
+    axs_flat[0].set_title('Subplot 1: Generator Loss')
 
-axs_flat[2].set_title('Subplot 3: Gradient Pentalty')
+    axs_flat[1].plot(steps_dis, losses_dis, label='Critic Loss')
+    axs_flat[1].plot(steps_dis, moving_avg(data=losses_dis, window_size=10)[9:-9], linestyle='--', label='Centered MA')
+    axs_flat[1].hlines(y=0, xmin=steps_dis[0]-1e2, xmax=steps_dis[-1]+1e2, linestyle='--', color='r', alpha=0.6)
 
-wd = np.array(losses_fake)-np.array(losses_real)
+    axs_flat[1].set_xlabel('Iteration')
+    axs_flat[1].set_ylabel('Loss Score')
+    axs_flat[1].set_xlim(-5, steps_dis[-1]+5)
 
-axs_flat[3].plot(steps_fake, losses_fake, label='Fake samples')
-axs_flat[3].plot(steps_real, losses_real, label='Real samples')
-# axs_flat[3].plot(steps_real, wd, color='grey', label='1-Wassertein Dist.')
-axs_flat[3].fill_between(
-    steps_fake,           # X-axis
-    losses_fake,          # Upper boundary
-    losses_real,          # Lower boundary
-    color='gray',         # Color of the shading
-    alpha=0.2,            # Transparency (0.0 to 1.0)
-    label='Distinction Gap'
-)
-axs_flat[3].hlines( y=0, xmin=steps_gp[0]-1e2, xmax=steps_gp[-1]+1e2, linestyle='--', color='r', alpha=0.6)
+    axs_flat[1].legend()
+    axs_flat[1].set_title('Subplot 2: Critic Loss')
 
-axs_flat[3].set_xlabel('Iteration')
-axs_flat[3].set_ylabel('Loss Score')
-axs_flat[3].set_xlim(-5, steps_gp[-1]+5)
+    axs_flat[2].plot(steps_gp, losses_gp, label='')
+    axs_flat[2].hlines(y=0, xmin=steps_gp[0]-1e2, xmax=steps_gp[-1]+1e2, linestyle='--', color='r', alpha=0.6)
 
-axs_flat[3].legend()
-axs_flat[3].set_title('Subplot 4: Real Sample Loss vs Fake Sample Loss')
+    axs_flat[2].set_xlabel('Iteration')
+    axs_flat[2].set_ylabel('Penalty Norm')
+    axs_flat[2].set_xlim(-5, steps_gp[-1]+5)
 
-plt.tight_layout()
-plt.show()
+    axs_flat[2].set_title('Subplot 3: Gradient Pentalty')
 
-# Save img
-path = os.path.join(image_dir, 'loss_curves.png')
-plt.savefig(path, format="PNG")
+    wd = np.array(losses_fake)-np.array(losses_real)
+
+    axs_flat[3].plot(steps_fake, losses_fake, label='Fake samples')
+    axs_flat[3].plot(steps_real, losses_real, label='Real samples')
+    # axs_flat[3].plot(steps_real, wd, color='grey', label='1-Wassertein Dist.')
+    axs_flat[3].fill_between(
+        steps_fake,           # X-axis
+        losses_fake,          # Upper boundary
+        losses_real,          # Lower boundary
+        color='gray',         # Color of the shading
+        alpha=0.2,            # Transparency (0.0 to 1.0)
+        label='Distinction Gap'
+    )
+    axs_flat[3].hlines( y=0, xmin=steps_gp[0]-1e2, xmax=steps_gp[-1]+1e2, linestyle='--', color='r', alpha=0.6)
+
+    axs_flat[3].set_xlabel('Iteration')
+    axs_flat[3].set_ylabel('Loss Score')
+    axs_flat[3].set_xlim(-5, steps_gp[-1]+5)
+
+    axs_flat[3].legend()
+    axs_flat[3].set_title('Subplot 4: Real Sample Loss vs Fake Sample Loss')
+
+    plt.tight_layout()
+    plt.show()
+
+    # Save img
+    path = os.path.join(image_dir, 'loss_curves.png')
+    plt.savefig(path, format="PNG")
+
+if __name__ == "__main__":
+    main()
